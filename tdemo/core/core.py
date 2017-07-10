3106768d44f84e5624ae924c927db90d5559cfd6
Jude <jude@wrapp.com>
2017-07-11 00:17:28 +0200

    Don't expose cf client, Fix tests

--- tdemo/core/core.py
+++ tdemo/core/core.py
@@ -4,14 +4,11 @@ import boto3
 import botocore
 
 
-# The CloudFormation client to create, update and delete stack
-cf = boto3.client('cloudformation')
-
-
 def _stack_exists(stack_name):
     """ Checks if the stack exists.
     Returns True if it exists and False if not.
     """
+    cf = boto3.client('cloudformation')
     exists = False
     try:
         cf.describe_stacks(StackName=stack_name)
@@ -28,6 +25,7 @@ def create_stack(stack_name, template):
     """ Creates the stack given the template. """
     if _stack_exists(stack_name):
         raise Exception("Stack exists, stack_name=%s" % stack_name)
+    cf = boto3.client('cloudformation')
     resp = cf.create_stack(StackName=stack_name, TemplateBody=template)
     return resp['StackId']
 
@@ -36,10 +34,14 @@ def update_stack(stack_name, template):
     """ Updates the stack with the updated template. """
     if not _stack_exists(stack_name):
         raise Exception("Stack does not exists, stack_name=%s" % stack_name)
+    cf = boto3.client('cloudformation')
     resp = cf.update_stack(StackName=stack_name, TemplateBody=template)
     return resp['StackId']
 
 
 def delete_stack(stack_name):
     """ Deletes the stack. """
+    if not _stack_exists(stack_name):
+        raise Exception("Stack does not exists, stack_name=%s" % stack_name)
+    cf = boto3.client('cloudformation')
     cf.delete_stack(StackName=stack_name)
--- tests_dev/test_core.py
+++ tests_dev/test_core.py
@@ -1,15 +1,19 @@
+import os
 import json
 import nose.tools as nt
 from mock import patch, ANY, Mock
 
 import botocore
+from botocore.stub import Stubber
 
 from tdemo.core import core
 from tdemo import stack_dev
 
 
 class TestCore(object):
+
     def setup(self):
+        os.environ['AWS_DEFAULT_REGION'] = 'eu-west-1'
         with open("tests_dev/worksample.cfn.template") as f:
             self.expected_template = f.read()
 
@@ -18,47 +22,47 @@ class TestCore(object):
         nt.assert_equals(sorted(json.loads(template)),
                          sorted(json.loads(self.expected_template)))
 
-    @patch('tdemo.core.core.cf')
     @patch('tdemo.core.core._stack_exists')
-    def test_successfully_creates_stack(self, mock_stack_exists, mock_cf):
+    @patch('botocore.client.BaseClient._make_api_call')
+    def test_successfully_creates_stack(self, mock_botoapi, mock_stack_exists):
         mock_stack_exists.return_value = False
         core.create_stack("MyStack", "{}")
-        nt.assert_equals(mock_cf.create_stack.call_count, 1)
+        nt.assert_equals(mock_botoapi.call_count, 1)
 
-    @patch('tdemo.core.core.cf')
     @patch('tdemo.core.core._stack_exists')
+    @patch('botocore.client.BaseClient._make_api_call')
     @nt.raises(Exception)
-    def test_fails_to_create_stack_if_exists(self, mock_stack_exists, mock_cf):
+    def test_fails_to_create_stack_if_exists(self, mock_botoapi, mock_stack_exists):
         mock_stack_exists.return_value = True
         core.create_stack("MyStack", "{}")
-        nt.assert_equals(mock_cf.create_stack.call_count, 0)
+        nt.assert_equals(mock_botoapi.call_count, 0)
 
-    @patch('tdemo.core.core.cf')
     @patch('tdemo.core.core._stack_exists')
-    def test_successfully_updates_stack(self, mock_stack_exists, mock_cf):
+    @patch('tdemo.core.core.boto3.client')
+    def test_successfully_updates_stack(self, mock_botoapi, mock_stack_exists):
         mock_stack_exists.return_value = True
         core.update_stack("MyStack", "{}")
-        nt.assert_equals(mock_cf.update_stack.call_count, 1)
+        nt.assert_equals(mock_botoapi.call_count, 1)
 
-    @patch('tdemo.core.core.cf')
     @patch('tdemo.core.core._stack_exists')
+    @patch('tdemo.core.core.boto3.client')
     @nt.raises(Exception)
-    def test_fails_to_update_stack_if_does_not_exist(self, mock_stack_exists, mock_cf):
+    def test_fails_to_update_stack_if_does_not_exist(self, mock_botoapi, mock_stack_exists):
         mock_stack_exists.return_value = False
         core.update_stack("MyStack", "{}")
-        nt.assert_equals(mock_cf.update_stack.call_count, 0)
+        nt.assert_equals(mock_botoapi.call_count, 0)
 
-    @patch('tdemo.core.core.cf')
     @patch('tdemo.core.core._stack_exists')
-    def test_successfully_deletes_stack(self, mock_stack_exists, mock_cf):
+    @patch('tdemo.core.core.boto3.client')
+    def test_successfully_deletes_stack(self, mock_botoapi, mock_stack_exists):
         mock_stack_exists.return_value = True
         core.delete_stack("MyStack")
-        nt.assert_equals(mock_cf.delete_stack.call_count, 1)
+        nt.assert_equals(mock_botoapi.call_count, 1)
 
-    @patch('tdemo.core.core.cf')
     @patch('tdemo.core.core._stack_exists')
+    @patch('tdemo.core.core.boto3.client')
     @nt.raises(Exception)
-    def test_fails_to_delete_stack_if_does_not_exist(self, mock_stack_exists, mock_cf):
+    def test_fails_to_delete_stack_if_does_not_exist(self, mock_botoapi, mock_stack_exists):
         mock_stack_exists.return_value = False
         core.delete_stack("MyStack")
-        nt.assert_equals(mock_cf.delete_stack.call_count, 0)
+        nt.assert_equals(mock_botoapi.call_count, 0)
